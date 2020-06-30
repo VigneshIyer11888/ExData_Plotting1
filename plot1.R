f9c9d63552eb5b297b2c6e935ad7bf2d8419a708
VigneshIyer11888 <vigneshiyer11888@gmail.com>
2020-06-30 23:40:01 +0530

    Commit Files

---
+++ plot1.R
@@ -0,0 +1,24 @@
+# Checking if the directory exists or not and creating it if
+# it doesn't exists. Once it has been created download the zip
+# file and extracting the data
+
+setwd("C:/Users/DELL/OneDrive/Desktop/Coursera Folder")
+
+if(!file.exists("./Exploratory Data Analysis Project-1")){
+  dir.create("./Exploratory Data Analysis Project-1")
+}
+
+datafile <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+download.file(datafile, 
+destfile = "./Exploratory Data Analysis Project-1/exdata_data_household_power_consumption.zip")
+
+unzip(zipfile = "./Exploratory Data Analysis Project-1/exdata_data_household_power_consumption.zip", 
+exdir = "./Exploratory Data Analysis Project-1/exdata_data_household_power_consumption")
+
+# Reading the text file
+
+powerCondata <- read.csv("./Exploratory Data Analysis Project-1/exdata_data_household_power_consumption/household_power_consumption.txt",
+                         sep = ";",
+                         na.strings = "?" )
+
